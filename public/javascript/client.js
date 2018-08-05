aca1f184e81d2486944bcd07f5cc1d7e866f03b0
Pawel <pawelp73@gmail.com>
2018-08-05 18:04:58 +0200

    front end improvements

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
-// TODO
-// resolving errors
+// TODO resolving errors
 console.log('Hello!');
 const urlLink = 'http://localhost:3000/link/';
 const responseContainer = document.querySelector('ul');
@@ -13,60 +12,103 @@ let model = {
   current: false,
   toggleCurrent: function (caller) {
     if (this.current) {
-      this.current === caller ? this.current = false : this.current = caller;
-    }
-    else {
+      this.current === caller
+        ? this.current = false
+        : this.current = caller;
+    } else {
       this.current = caller;
     }
-  },
+  }
 };
 let view = {
   renderArticle: caller => {
     caller.insertAdjacentHTML('beforeend', htmlContent);
-    view.adjustColors.check(caller);
+    view
+      .adjustColors
+      .check(caller);
     controller.removePlaceholder(caller);
     model.pending = false;
     model.current = caller;
   },
   adjustColors: {
     visited: caller => {
-      caller.classList.add('visited');
-      caller.classList.remove('active');
+      caller
+        .classList
+        .add('visited');
+      caller
+        .classList
+        .remove('active');
     },
     active: caller => {
-      caller.classList.add('active');
-      caller.classList.remove('visited');
+      caller
+        .classList
+        .add('active');
+      caller
+        .classList
+        .remove('visited');
     },
     activate: caller => {
-      caller.classList.add('active');
-      caller.classList.remove('standard');
+      caller
+        .classList
+        .add('active');
+      caller
+        .classList
+        .remove('standard');
     },
     check: function (caller) {
       if (caller.classList.contains('standard')) {
         this.activate(caller);
-      }
-      else if (caller.classList.contains('active')) {
+      } else if (caller.classList.contains('active')) {
         this.visited(caller);
-      }
-      else {
+      } else {
         this.active(caller);
       }
     }
   },
   toggleArticle: caller => {
-    caller.style.display != "none" ? caller.style.display = "none" : caller.style.display = "block";
+    caller.style.display != "none"
+      ? caller.style.display = "none"
+      : caller.style.display = "block";
   },
-
   clearCurrent: caller => {
     if (model.current && model.current !== caller) {
-      let articleDisplay = model.current.querySelector('.article-content');
+      let articleDisplay = model
+        .current
+        .querySelector('.article-content');
       view.toggleArticle(articleDisplay);
-      view.adjustColors.visited(model.current);
+      view
+        .adjustColors
+        .visited(model.current);
     }
+  },
+  composeText: function () {
+    setTimeout(() => {
+      this.displayLetters(
+        'serverside node.js webscraper built with puppeteer',
+        'h6',
+        60
+      );
+    }, 3000);
+    this.displayLetters('Scrape it!', 'h4', 160);
+  },
+  displayLetters: (text, elem, delay) => {
+    let mySelector = document.getElementsByTagName(elem)[0];
+    let splitText = text.split("");
+    for (let letter = 0; letter < splitText.length; letter++) {
+      setTimeout(() => {
+        splitText[letter] !== " "
+          ? mySelector.innerHTML += splitText[letter]
+          : mySelector.innerHTML += "&nbsp;";
+      }, delay * (letter + 1));
+    };
+    return;
   }
 }
 let controller = {
-  init: function () { this.listenersOn() },
+  init: function () {
+    this.listenersOn();
+    view.composeText();
+  },
   listenersOn: () => {
     for (let article of articleList) {
       article.addEventListener("click", controller.listenClick);
@@ -78,17 +120,19 @@ let controller = {
       if (innerElem) {
         if (model.current === false || model.current === this) {
           view.toggleArticle(innerElem);
-          view.adjustColors.check(this);
+          view
+            .adjustColors
+            .check(this);
           model.toggleCurrent(this);
-        }
-        else {
+        } else {
           view.clearCurrent(this);
           view.toggleArticle(innerElem);
-          view.adjustColors.check(this);
+          view
+            .adjustColors
+            .check(this);
           model.toggleCurrent(this);
         }
-      }
-      else {
+      } else {
         this.insertAdjacentHTML('beforeend', '<div class ="loader"><div>');
         model.pending = 'true';
         controller.getArticle(this);
@@ -98,17 +142,20 @@ let controller = {
   },
   getArticle: function (caller) {
     fetch(`${urlLink}?link=${caller.dataset.link}`)
-      .then(response => response.json())
       .then(
-        data => {
-          this.addArticle(data);
-          view.renderArticle(caller);
-        }
+        response => response.json()
       )
+      .then(data => {
+        this.addArticle(data);
+        view.renderArticle(caller);
+      })
   },
   addArticle: data => {
-    htmlContent = data.articleEntry.map(articles => `
-  <p class = 'article-part'>${articles.article}</p>`).join('');
+    htmlContent = data
+      .articleEntry
+      .map(articles => `
+  <p class = 'article-part'>${articles.article}</p>`)
+      .join('');
     htmlContent = `<ul class ='article-content'> ${htmlContent}</ul>`;
   },
   removePlaceholder: function (caller) {
